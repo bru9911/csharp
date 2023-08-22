b63c2c0b11c3c66f3d8c69e19d642145822a2d3d
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2023-08-22 16:48:51 -0300

    Create pesquicaindentificadorlista.cs

---
+++ pesquicaindentificadorlista.cs
@@ -0,0 +1,20 @@
+var index = names.IndexOf("Felipe");
+if (index == -1)
+{
+    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+}
+else
+{
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+}
+
+index = names.IndexOf("Not Found");
+if (index == -1)
+{
+    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+}
+else
+{
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+
+}
